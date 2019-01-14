5532ab21dbb8b0707d4dee99a69ba81d08231d05
Liron Zadkovsky <lironz85@gmail.com>
2019-01-14 07:29:32 +0200

    Search by case number does not work #679

--- imports/ui/case/case-search.actions.js
+++ imports/ui/case/case-search.actions.js
@@ -1,11 +1,14 @@
 export const UPDATE_SEARCH_RESULT = 'update_search_result'
 export const FINISH_SEARCH = 'finish_search'
 export const START_SEARCH = 'start_search'
+export const NAVIGATION_REQUESTED = 'navigation_requested'
+export const NAVIGATION_GRANTED = 'navigation_granted'
 
 export function updateSearch (searchText) {
   return {
     type: UPDATE_SEARCH_RESULT,
-    searchText: searchText
+    searchText: searchText,
+    navigationRequested: false
   }
 }
 
@@ -20,5 +23,19 @@ export function startSearch () {
   return {
     type: START_SEARCH,
     searchFinished: false
+  }
+}
+
+export function navigationRequested () {
+  return {
+    type: NAVIGATION_REQUESTED,
+    navigationRequested: true
+  }
+}
+
+export function navigationGranted () {
+  return {
+    type: NAVIGATION_GRANTED,
+    navigationRequested: false
   }
 }
